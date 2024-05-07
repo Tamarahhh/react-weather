ae427a18f45b22d83977909c0bb9dab713e5708d
Tamara Boboye <160331279+Tamarahhh@users.noreply.github.com>
2024-05-07 16:38:28 +0100

    updated footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,8 +5,10 @@ export default function Footer() {
     <div className="Footer">
       <p>
         {" "}
-        This project was coded by <a href="">Tamara Boboye</a> and is on{" "}
-        <a href="">Github</a> and hosted on <a href="">Netlify.</a>
+        This project was coded by{" "}
+        <a href="https://github.com/Tamarahhh">Tamara Boboye</a> and is on{" "}
+        <a href="https://github.com/Tamarahhh/react-weather">Github</a> and
+        hosted on Netlify.
       </p>
     </div>
   );
