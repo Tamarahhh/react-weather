04f47c5356631ce93cdfe3cc89dbdcbd292572da
Tamara Boboye <160331279+Tamarahhh@users.noreply.github.com>
2024-05-07 16:43:51 +0100

    added a link to footer

--- src/Footer.js
+++ src/Footer.js
@@ -8,7 +8,8 @@ export default function Footer() {
         This project was coded by{" "}
         <a href="https://github.com/Tamarahhh">Tamara Boboye</a> and is on{" "}
         <a href="https://github.com/Tamarahhh/react-weather">Github</a> and
-        hosted on Netlify.
+        hosted on{" "}
+        <a href="https://chic-platypus-4abf03.netlify.app/">Netlify</a>.
       </p>
     </div>
   );
